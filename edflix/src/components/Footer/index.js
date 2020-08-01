02f827ef6f2a8a4dc81f5bb5181d7f270ca3b054
edsoncarlosdvp <edsoncarlos@equipenovainfo.com.br>
2020-08-01 02:27:03 -0300

    Dia 01 - App estruturado

---
+++ edflix/src/components/Footer/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { FooterBase } from './styles';
+
+function Footer() {
+  return (
+    <FooterBase>
+      <a href="https://www.alura.com.br/">
+        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      </a>
+      < p >
+        Adaptação: Edson Carlos.
+      </p>
+      <p>
+        Orgulhosamente criado durante a
+        {' '}
+        <a href="https://www.alura.com.br/">
+          Imersão React da Alura
+        </a>
+      </p>
+    </FooterBase>
+  );
+}
+
+export default Footer;
